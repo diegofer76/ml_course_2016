034ea620ae572b47aa0ba1ba652d9eda0e80ddb9
Diego Fernandez Merjildo <merjildo@br.ibm.com>
2016-10-03 12:19:13 -0300

    ex_02: first version, intern fold
    Signed-off-by: Diego Fernandez Merjildo <merjildo@br.ibm.com>

--- exercises/exercise_02/svm_ex_02.py
+++ exercises/exercise_02/svm_ex_02.py
@@ -3,6 +3,9 @@
 import sys,os,csv
 import pandas
 import numpy as np
+from sklearn import cross_validation
+from sklearn.cross_validation import StratifiedKFold
+
 datFileName="data1.csv"
 dirPath=os.path.dirname(os.path.realpath(__file__))
 classList=[]
@@ -42,6 +45,39 @@ def getClassTrainTest(classList):
     print len(classListTrain), len(classListTest)
     return [classListTrain, classListTest]
 
+# A validacao externa deve ser 5-fold estratificado.
+def externFolds(classList):
+    [data_train, data_test] = getTrainAndTestData(data)
+    [classListTrain, classListTest] = getClassTrainTest(classList)
+
+    skf = cross_validation.StratifiedKFold(classList, n_folds=5)
+    # modelo = fit(data_train, hmax)
+	# ac = acuracia(modelo, data_test)
+
+
+# In order to get hyperparameter
+def internFolds(data, classList):
+    [data_train, data_test] = getTrainAndTestData(data)
+    [labelsTrain, labelsTest] = getLabelsTrainTest(classList)
+    
+    # MDOX =  fit (data_train, H)
+    best_acc = 0
+    for c in [2**-5, 2**-2, 1, 2**2, 2**5]:
+        for gam in [2**-15, 2**-10, 2**-5, 1, 2**5]:
+            svm = SVM.SVC(C = c, gamma = gam)
+            svm.fit(data_train, labelsTrain)
+            accuracy = svm.score(data_test, labelsTest)
+        if accuracy > best_acc:
+            best_acc = accuracy
+    print("Best accuracy with 3-fold:", best_acc)
+    # ACX = ACX + acuracia(mdox, data_test)
+    # if acx > acxmax:
+    #    acmax = acx
+    #    hmax = h
+
+def nanda():
+    print 'nn'
+
 def main(argv=None):
     if argv is None:
         arv = sys.argv
@@ -49,6 +85,13 @@ def main(argv=None):
     [data, classList] = getData(rawdata)
     [data_train, data_test] = getTrainAndTestData(data)
     [classListTrain, classListTest] = getClassTrainTest(classList)
+
+    skf = cross_validation.StratifiedKFold(classList, n_folds=5)
+    for train_index, test_index in skf:
+        new_data_train = data[train_index]
+        print "len: " , len(new_data_train)
+
+    print "-->", skf
 
 if __name__ == "__main__":
     sys.exit(main())

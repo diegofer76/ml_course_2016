1de6042737ab96f1e0251b30ed64eaea4107af88
diegofer76 <diegoaqp@gmail.com>
2016-10-03 23:42:17 -0300

    ex02 : Cleaning code
    Signed-off-by: diegofer76 <diegoaqp@gmail.com>

--- exercises/exercise_02/svm_ex_02.py
+++ exercises/exercise_02/svm_ex_02.py
@@ -22,29 +22,10 @@ def getData(rawData):
     #print "\n---- Getting data from File ----"
     lineNum = rawData.shape[0]
     colNum = rawData.shape[1]
-    #print "lineNum:", lineNum
-    #print "colNum:", colNum
-
     data = np.array(rawData[0:lineNum, 0:colNum-1])
     for i in range(lineNum):
         classList.append(rawData[i][colNum - 1])
     return [data, np.array(classList) ]
-
-def getTrainAndTestData(data):
-    #print "\n---- Get Train and Test data ----"
-    data_train = data[0:199]
-    data_test = data[200:data.shape[0]]
-
-    #print "Data Train size:", data_train.shape 
-    #print "Data Test size:", data_test.shape
-    return [data_train, data_test]
-
-def getLabelsTrainTest(classList):
-    #print "\n---- Get Class Train and Test ----"
-    classListTrain=classList[0:199]
-    classListTest=classList[200:len(classList)]
-    #print len(classListTrain), len(classListTest)
-    return [classListTrain, classListTest]
 
 # In order to get hyperparameter
 def internFolds(data_train, data_test, labelsTrain, labelsTest):
@@ -67,8 +48,6 @@ def main(argv=None):
         arv = sys.argv
     rawdata = loadCsvData(dirPath + "/" + datFileName)
     [data, labels] = getData(rawdata)
-    [data_train, data_test] = getTrainAndTestData(data)
-    [labels_train, labels_test] = getLabelsTrainTest(labels)
     final_accuracy = 0
 
     skf = cross_validation.StratifiedKFold(labels, n_folds=5)

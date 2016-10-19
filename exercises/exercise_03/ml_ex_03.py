ade48d2ee606746bb9e7cb3b615751d0d217ae5a
diegofer76 <diegoaqp@gmail.com>
2016-10-18 23:24:20 -0200

    ex03: first part - load data
    Signed-off-by: diegofer76 <diegoaqp@gmail.com>

---
+++ exercises/exercise_03/ml_ex_03.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+import sys,os,csv
+import pandas
+import numpy as np
+import math
+from sklearn import cross_validation
+from sklearn.cross_validation import StratifiedKFold
+from sklearn import svm as SVM
+
+datFileName="secom.data"
+#datFileName="../exercise_02/data1.csv"
+dirPath=os.path.dirname(os.path.realpath(__file__))
+classList=[]
+data=[]
+
+def load_data(fileName):
+    raw_data = open(fileName, 'rb')
+    rawData = pandas.read_csv(raw_data, delimiter=" ")
+    return rawData.values
+
+def getData(rawData):
+    #print "\n---- Getting data from File ----"
+    lineNum = rawData.shape[0]
+    colNum = rawData.shape[1]
+    data = np.array(rawData[0:lineNum, 0:colNum-1])
+    for i in range(lineNum):
+        classList.append(rawData[i][colNum - 1])
+    return [data, np.array(classList) ]
+
+# In order to get hyperparameter
+def internFolds(data_train, data_test, labelsTrain, labelsTest):
+    acxmax = 0
+    c_max=0
+    gamma_max=0
+    for c in [2**-5, 2**-2, 1, 2**2, 2**5]:
+        for gamm in [2**-15, 2**-10, 2**-5, 1, 2**5]:
+            svm = SVM.SVC(C = c, gamma = gamm)
+            svm.fit(data_train, labelsTrain)
+            accuracy = svm.score(data_test, labelsTest)
+            if accuracy > acxmax:
+                acxmax = accuracy
+                c_max = c
+                gamma_max = gamm
+    return [acxmax, c_max, gamma_max]
+
+def svm_rbf(data, labels):
+    final_accuracy = 0
+    skf = cross_validation.StratifiedKFold(labels, n_folds=5)
+    for train_index, test_index in skf:
+        new_data_train = data[train_index]
+        new_data_test = data[test_index]
+        new_labels_train = labels[train_index]
+        new_labels_test = labels[test_index]
+
+        acx = 0
+        skf_intern = cross_validation.StratifiedKFold(new_labels_train, n_folds=3)
+        for intern_train_index, intern_test_index in skf_intern:
+            intern_data_train = new_data_train[intern_train_index]	
+            intern_data_test = new_data_train[intern_test_index]	
+            intern_labels_train = new_labels_train[intern_train_index]	
+            intern_labels_test = new_labels_train[intern_test_index]
+            [accuracy, c, gamma] = internFolds(intern_data_train, intern_data_test, intern_labels_train, intern_labels_test)
+            if accuracy > acx:
+                acx = accuracy 
+                c_final = c
+                gamma_final = gamma
+        #print("acx", acx)        
+        print("Valor Hiperparametros (C=%s, Gamma=%s)" % (c_final, gamma_final) )
+        svm_model = SVM.SVC(C = c_final, gamma = gamma_final)
+        svm_model.fit(new_data_train, new_labels_train)
+        acc_5_fold = svm_model.score(new_data_test, new_labels_test)
+        acc_5_fold = svm_model.score(new_data_test, new_labels_test)
+        final_accuracy = final_accuracy + acc_5_fold
+        #final_accuracy = final_accuracy + acx
+
+    final_accuracy = final_accuracy / 5
+    print("Acuracia media:%s" % final_accuracy)
+    print("Valor final hiperparametros (C=%s, Gamma=%s)" % (c_final, gamma_final) )
+
+
+def main(argv=None):
+    if argv is None:
+        arv = sys.argv
+
+    ## Data pre-processing    
+    rawdata = load_data(dirPath + "/" + datFileName)
+    col_med = np.zeros(shape=(rawdata.shape[1],1))
+    for line in rawdata:
+        print "\n \n"
+        #col_med=0
+        for i in range(len(line)):
+            if math.isnan(float(line[i])):
+                line[i] = 0
+                print " ", str(float(line[i]))
+            else:
+                col_med[i] += float(line[i])
+                print str(float(line[i])), 
+
+    for i in range(len(col_med)):
+        col_med[i] /= rawdata.shape[0]
+
+    print "col_med: ", col_med
+        #print "\n\n" + str(line) + "<:"
+
+    #[data, labels] = getData(rawdata)
+    print rawdata.shape
+
+    ## kNN , PCA com 80% da variancia
+
+
+    ## SVM RBF 
+    #svm_rbf(data, labels)
+
+
+if __name__ == "__main__":
+    sys.exit(main())

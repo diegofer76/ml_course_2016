0a3be49d3477301e109210b0268214fbc4e66ede
Diego Fernandez Merjildo <merjildo@br.ibm.com>
2016-10-21 17:49:12 -0200

    ex03 : Knn and svm working. Neural network need sklearn 0.18
    Signe-off-by: Diego Fernandez Merjildo <merjildo@br.ibm.com>

--- exercises/exercise_03/ml_ex_03.py
+++ exercises/exercise_03/ml_ex_03.py
@@ -8,10 +8,12 @@ from sklearn import cross_validation
 from sklearn.cross_validation import StratifiedKFold
 from sklearn import svm as SVM
 import  scipy.stats as stats
+from sklearn.neighbors import KNeighborsClassifier
+
+#from sklearn.neural_network import MLPClassifier
 
 datFileName="secom.data"
 labelsFileName="secom_labels.data"
-#datFileName="../exercise_02/data1.csv"
 dirPath=os.path.dirname(os.path.realpath(__file__))
 classList=[]
 data=[]
@@ -19,7 +21,6 @@ data=[]
 def load_data(fileName):
     raw_data = open(fileName, 'rb')
     rawData = pandas.read_csv(raw_data, delimiter=" ")
-    print "load_data", rawData.shape
     return rawData.values
 
 def getData(rawData):
@@ -31,24 +32,6 @@ def getData(rawData):
         classList.append(rawData[i][colNum - 1])
     return [data, np.array(classList) ]
 
-## Data preprocessing
-def data_preprocess(fileName):
-    rawdata = load_data(dirPath + "/" + fileName)
-    ## column mean
-    column_mean = stats.nanmean(rawdata, axis=0)
-    ## Nan values index
-    nan_indexes = np.where(np.isnan(rawdata))
-    ## Replace Nan values
-    rawdata[nan_indexes] = np.take(column_mean, nan_indexes[1])
-    ## Standarize each column individually
-    rawdata = (rawdata - np.mean(rawdata, axis=0)) / np.std(rawdata, axis=0)
-    rawdata = np.nan_to_num(rawdata)
-
-    np.savetxt("foo.csv", rawdata, delimiter=",")
-    csv_data = load_data("foo.csv")
-    return csv_data
-    #return np.array(csv_data)
-
 def getLabels(fileName):
     labelData = load_data(dirPath + "/" + fileName)
     labels = labelData[:,0].clip(min=0)
@@ -56,12 +39,27 @@ def getLabels(fileName):
 
 
 # In order to get hyperparameter
-def internFolds(data_train, data_test, labelsTrain, labelsTest):
+#def internFolds(data_train, data_test, labelsTrain, labelsTest):
+#    acxmax = 0
+#    c_max=0
+#    gamma_max=0
+#    for c in [2**-5, 2**-2, 1, 2**2, 2**5]:
+#        for gamm in [2**-15, 2**-10, 2**-5, 1, 2**5]:
+#            svm = SVM.SVC(C = c, gamma = gamm)
+#            svm.fit(data_train, labelsTrain)
+#            accuracy = svm.score(data_test, labelsTest)
+#            if accuracy > acxmax:
+#                acxmax = accuracy
+#                c_max = c
+#                gamma_max = gamm
+#    return [acxmax, c_max, gamma_max]
+
+def svm_intern_folds(data_train, data_test, labelsTrain, labelsTest):
     acxmax = 0
     c_max=0
     gamma_max=0
-    for c in [2**-5, 2**-2, 1, 2**2, 2**5]:
-        for gamm in [2**-15, 2**-10, 2**-5, 1, 2**5]:
+    for c in [2**(-5), 1, 2**(5), 2**(10)]:
+        for gamm in [2**(-15), 2**(-10), 2**(-5), 1, 2**5]:
             svm = SVM.SVC(C = c, gamma = gamm)
             svm.fit(data_train, labelsTrain)
             accuracy = svm.score(data_test, labelsTest)
@@ -71,7 +69,109 @@ def internFolds(data_train, data_test, labelsTrain, labelsTest):
                 gamma_max = gamm
     return [acxmax, c_max, gamma_max]
 
-def svm_rbf(data, labels):
+def knn_intern_folds(data_train, data_test, labels_train, labels_test):
+    acxmax = 0
+    cores = 4
+    k_value = 0
+    for k in [1, 5, 11, 15, 21, 25]:
+        knn = KNeighborsClassifier(n_neighbors = k, n_jobs = cores)
+        knn.fit(data_train, labels_train)
+        accuracy = knn.score(data_test, labels_test)
+        #print "first acc:", accuracy
+        if accuracy > acxmax:
+            acxmax = accuracy
+            k_value = k
+            #print "accuracy: ", accuracy
+    return [acxmax, k]
+
+def neural_intern_folds(data_train, data_test, labelsTrain, labelsTest):
+    # 10, 20, 30 e 40 neuronios na camada escondida.
+    acxmax = 0
+    cores = 4
+    n_value = 0
+    for n in [10, 20, 30, 40]:
+        clf = MLPClassifier(hidden_layer_sizes=(n,))
+        clf.fit(data_train, labels_train)
+        accuracy = clf.score(data_test, labels_test)
+        #print "first acc:", accuracy
+        if accuracy > acxmax:
+            acxmax = accuracy
+            n_value = n
+            print "accuracy: ", accuracy
+    return [acxmax, n]
+
+def rf_intern_folds(data_train, data_test, labelsTrain, labelsTest):
+    # teste com mtry ou n_featrues = 10, 15, 20, 25 e ntrees = 100, 200, 300 e 400
+    print ""
+
+#def svm_rbf(data, labels):
+#    final_accuracy = 0
+#    skf = cross_validation.StratifiedKFold(labels, n_folds=5)
+#    for train_index, test_index in skf:
+#        new_data_train = data[train_index]
+#        new_data_test = data[test_index]
+#        new_labels_train = labels[train_index]
+#        new_labels_test = labels[test_index]
+
+#        acx = 0
+#        skf_intern = cross_validation.StratifiedKFold(new_labels_train, n_folds=3)
+#        for intern_train_index, intern_test_index in skf_intern:
+#            intern_data_train = new_data_train[intern_train_index]
+#            intern_data_test = new_data_train[intern_test_index]
+#            intern_labels_train = new_labels_train[intern_train_index]
+#            intern_labels_test = new_labels_train[intern_test_index]
+#            [accuracy, c, gamma] = internFolds(intern_data_train, intern_data_test, intern_labels_train, intern_labels_test)
+#
+#            if accuracy > acx:
+#                acx = accuracy
+#                c_final = c
+#                gamma_final = gamma
+        ##print("acx", acx)
+#        print("Valor Hiperparametros (C=%s, Gamma=%s)" % (c_final, gamma_final) )
+#        svm_model = SVM.SVC(C = c_final, gamma = gamma_final)
+#        svm_model.fit(new_data_train, new_labels_train)
+#        acc_5_fold = svm_model.score(new_data_test, new_labels_test)
+#        acc_5_fold = svm_model.score(new_data_test, new_labels_test)
+#        final_accuracy = final_accuracy + acc_5_fold
+##        final_accuracy = final_accuracy + acx
+
+#    final_accuracy = final_accuracy / 5
+#    print("Acuracia media:%s" % final_accuracy)
+#    print("Valor final hiperparametros (C=%s, Gamma=%s)" % (c_final, gamma_final) )
+
+
+## Data preprocessing
+def data_preprocess(fileName):
+    rawdata = load_data(dirPath + "/" + fileName)
+    ## column mean
+    column_mean = stats.nanmean(np.array(rawdata))
+    ## Nan values index
+    nan_indexes = np.where(np.isnan(rawdata))
+    ## Replace Nan values
+    rawdata[nan_indexes] = np.take(column_mean, nan_indexes[1])
+    ## Standarize each column individually
+    rawdata = (rawdata - np.mean(rawdata, axis=0)) / np.std(rawdata, axis=0)
+    rawdata = np.nan_to_num(rawdata)
+    return rawdata
+
+def model_score(alg, data, labels):
+    if 'svm'== alg:
+        print "svm"
+        [accuracy, c, gamma] = svm_intern_folds (intern_data_train, intern_data_test, intern_labels_train, intern_labels_test)
+        if accuracy > acx:
+            acx = accuracy
+            c_final = c
+            gamma_final = gamma
+
+        print("Valor Hiperparametros (C=%s, Gamma=%s)" % (c_final, gamma_final) )
+        final_accuracy = final_accuracy + acx
+
+    elif 'knn' == alg:
+        print 'knn'
+
+
+def run_svm_fold( data, labels):
+    print "--- SVM ---"
     final_accuracy = 0
     skf = cross_validation.StratifiedKFold(labels, n_folds=5)
     for train_index, test_index in skf:
@@ -79,7 +179,6 @@ def svm_rbf(data, labels):
         new_data_test = data[test_index]
         new_labels_train = labels[train_index]
         new_labels_test = labels[test_index]
-
         acx = 0
         skf_intern = cross_validation.StratifiedKFold(new_labels_train, n_folds=3)
         for intern_train_index, intern_test_index in skf_intern:
@@ -87,24 +186,73 @@ def svm_rbf(data, labels):
             intern_data_test = new_data_train[intern_test_index]	
             intern_labels_train = new_labels_train[intern_train_index]	
             intern_labels_test = new_labels_train[intern_test_index]
-            [accuracy, c, gamma] = internFolds(intern_data_train, intern_data_test, intern_labels_train, intern_labels_test)
+            [accuracy, c, gamma] = svm_intern_folds (intern_data_train, intern_data_test, intern_labels_train, intern_labels_test)
             if accuracy > acx:
                 acx = accuracy 
                 c_final = c
                 gamma_final = gamma
-        #print("acx", acx)        
-        print("Valor Hiperparametros (C=%s, Gamma=%s)" % (c_final, gamma_final) )
-        svm_model = SVM.SVC(C = c_final, gamma = gamma_final)
-        svm_model.fit(new_data_train, new_labels_train)
-        acc_5_fold = svm_model.score(new_data_test, new_labels_test)
-        acc_5_fold = svm_model.score(new_data_test, new_labels_test)
-        final_accuracy = final_accuracy + acc_5_fold
-        #final_accuracy = final_accuracy + acx
+        #print("Valor Hiperparametros (C=%s, Gamma=%s)" % (c_final, gamma_final) )
+        final_accuracy = final_accuracy + acx
 
     final_accuracy = final_accuracy / 5
     print("Acuracia media:%s" % final_accuracy)
     print("Valor final hiperparametros (C=%s, Gamma=%s)" % (c_final, gamma_final) )
 
+def run_knn_folds( data, labels):
+    print "--- Knn ---"
+    final_accuracy = 0
+    k_final = 0
+    skf = cross_validation.StratifiedKFold(labels, n_folds=5)
+    for train_index, test_index in skf:
+        new_data_train = data[train_index]
+        new_data_test = data[test_index]
+        new_labels_train = labels[train_index]
+        new_labels_test = labels[test_index]
+        acx = 0
+        skf_intern = cross_validation.StratifiedKFold(new_labels_train, n_folds=3)
+        for intern_train_index, intern_test_index in skf_intern:
+            intern_data_train = new_data_train[intern_train_index]
+            intern_data_test = new_data_train[intern_test_index]
+            intern_labels_train = new_labels_train[intern_train_index]
+            intern_labels_test = new_labels_train[intern_test_index]
+            [accuracy, k] = knn_intern_folds (intern_data_train, intern_data_test, intern_labels_train, intern_labels_test)
+            if accuracy > acx:
+                acx = accuracy
+                k_final = k
+        #print("Valor K neighbors (K=%s)" % (k_final) )
+        final_accuracy = final_accuracy + acx
+
+    final_accuracy = final_accuracy / 5
+    print("Acuracia media:%s" % final_accuracy)
+    print("Valor final K (K=%s)" % (k_final))
+
+def run_neural_folds( data, labels):
+    print "--- Neural Network ---"
+    final_accuracy = 0
+    k_final = 0
+    skf = cross_validation.StratifiedKFold(labels, n_folds=5)
+    for train_index, test_index in skf:
+        new_data_train = data[train_index]
+        new_data_test = data[test_index]
+        new_labels_train = labels[train_index]
+        new_labels_test = labels[test_index]
+        acx = 0
+        skf_intern = cross_validation.StratifiedKFold(new_labels_train, n_folds=3)
+        for intern_train_index, intern_test_index in skf_intern:
+            intern_data_train = new_data_train[intern_train_index]
+            intern_data_test = new_data_train[intern_test_index]
+            intern_labels_train = new_labels_train[intern_train_index]
+            intern_labels_test = new_labels_train[intern_test_index]
+            [accuracy, n] = neural_intern_folds (intern_data_train, intern_data_test, intern_labels_train, intern_labels_test)
+            if accuracy > acx:
+                acx = accuracy
+                n_final = n
+        print("Valor n neurons (N=%s)" % (n_final) )
+        final_accuracy = final_accuracy + acx
+
+    final_accuracy = final_accuracy / 5
+    print("Acuracia media:%s" % final_accuracy)
+    print("Valor final neurons (N=%s)" % (n_final))
 
 def main(argv=None):
     if argv is None:
@@ -113,17 +261,17 @@ def main(argv=None):
     ## Data pre-processing    
     data = data_preprocess(datFileName)
     labels = getLabels(labelsFileName)
-    labels = labels[:data.shape[0]]
-    
-    print "---------------" 
-    print "data shape:", data.shape
-    print "label shape:", labels.shape
+    labels = np.array(list(labels[:data.shape[0]]))
 
     ## kNN , PCA com 80% da variancia
+    #model_score('knn', data, labels)
+    run_knn_folds(data, labels)
 
     ## SVM RBF 
-    #svm_rbf(data, labels)
+    run_svm_fold(data, labels)
 
+    ## Neural network
+#    run_neural_folds(data, labels)
 
 if __name__ == "__main__":
     sys.exit(main())

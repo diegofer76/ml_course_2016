fac7bcf315911dc1bbcc742b71bcf21ae1544325
diegofer76 <diegoaqp@gmail.com>
2016-10-25 23:58:19 -0200

    ex04 : Add first version of kmeans running
    Signed-off-by: diegofer76 <diegoaqp@gmail.com>

--- exercises/exercise_04/kmeans_ex_04.py
+++ exercises/exercise_04/kmeans_ex_04.py
@@ -5,21 +5,21 @@ import pandas
 import numpy as np
 import math
 from sklearn.model_selection import StratifiedKFold
-from sklearn import svm as SVM
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.neural_network import MLPClassifier
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+import matplotlib.pyplot as plt
 
-datFileName="secom.data"
-labelsFileName="secom_labels.data"
+datFileName="cluster-data.csv"
+#labelsFileName="secom_labels.data"
 dirPath=os.path.dirname(os.path.realpath(__file__))
 classList=[]
 data=[]
 
 def load_data(fileName):
     raw_data = open(fileName, 'rb')
-    rawData = pandas.read_csv(raw_data, delimiter=" ")
+    #rawData = pandas.read_csv(raw_data, delimiter=" ")
+    rawData = pandas.read_csv(raw_data, delimiter=",", skiprows=1)
     return rawData.values
 
 def getData(rawData):
@@ -216,24 +216,56 @@ def main(argv=None):
         arv = sys.argv
 
     ## Data pre-processing    
-    data = data_preprocess(datFileName)
-    labels = getLabels(labelsFileName)
-    labels = np.array(list(labels[:data.shape[0]]))
+    # data = data_preprocess(datFileName)
+    data = load_data(datFileName)
+
+    data = scale(data)
+#     print data.shape
+#     print data
+#     kmeans = KMeans(init='k-means++', n_clusters=3, n_init=10)
+#     kmeans.fit(data)
+
+
+    reduced_data = PCA(n_components=2).fit_transform(data)
+    kmeans = KMeans(init='k-means++', n_clusters=3, n_init=10)
+    kmeans.fit(reduced_data)
+
+    # Step size of the mesh. Decrease to increase the quality of the VQ.
+    h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Obtain labels for each point in mesh. Use last trained model.
+    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure(1)
+    plt.clf()
+    plt.imshow(Z, interpolation='nearest',
+               extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+               cmap=plt.cm.Paired,
+               aspect='auto', origin='lower')
+
+    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+    # Plot the centroids as a white X
+    centroids = kmeans.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='x', s=169, linewidths=3,
+                color='w', zorder=10)
+    plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+              'Centroids are marked with white cross')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
 
     ## kNN , PCA com 80% da variancia
-    run_folds('knn', data, labels)
-
-    ## SVM RBF 
-    run_folds('svm', data, labels)
-
-    ## Neural network
-    run_folds('neural', data, labels)
-
-    ## RF
-    run_folds('rf', data, labels)
-
-    ## GBM
-    run_folds('gbm', data, labels)
+    #run_folds('knn', data, labels)
     
 if __name__ == "__main__":
     sys.exit(main())

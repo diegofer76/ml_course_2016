d9f1f7ec206ff76c35c81762662d6494e1e1bd5f
rodrigolrfm <rodrigotharo123@gmail.com>
2018-02-01 22:21:15 -0500

    solucion final del problema 2

--- coding_train/basics/calculeitor1.py
+++ coding_train/basics/calculeitor1.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 
+import sys
+
 def ejercicio1(x):
-    y = (x+4)^3
-	print str(y)
+    y = 6*(x**2) + 3*x + 2
+    print "el resultado es: " + str(y)
 
 def main():
 	x = 2
 	ejercicio1(x)
 
 if __name__ == "__main__":
-   sys.exit(main())
+    sys.exit(main())

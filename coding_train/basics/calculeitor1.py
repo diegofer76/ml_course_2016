73482d0afa94634a964d8047b78d61a4d441af19
rodrigolrfm <rodrigotharo123@gmail.com>
2018-02-01 22:44:52 -0500

    resolucion del problema 2.2

--- coding_train/basics/calculeitor1.py
+++ coding_train/basics/calculeitor1.py
@@ -6,9 +6,21 @@ def ejercicio1(x):
     y = 6*(x**2) + 3*x + 2
     print "el resultado es: " + str(y)
 
+def ejercicio2(x):
+    y = x**2
+    print "y( " + str(x) + " )" + " = " + str(y)
+
 def main():
-	x = 2
-	ejercicio1(x)
+    print "-----------ejercicio1------------"
+    x = 2
+    ejercicio1(x)
+
+    print "-----------ejercicio2------------"
+    x = 5
+    ejercicio2(x)
+
+    print "-----------ejercicio3------------"
+    x = (5+3)*4
 
 if __name__ == "__main__":
     sys.exit(main())

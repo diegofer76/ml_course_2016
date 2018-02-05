d14a8d522180a8e26c6d6137cf782577cd8e3e2c
Diego Fernandez-Merjildo <diegoaqp@gmail.com>
2018-02-05 13:21:26 -0200

    add hackerrank directory

---
+++ coding_train/hackerRank/easy/simple_array_sum.py
@@ -0,0 +1,14 @@
+
+#!/bin/python
+
+import sys
+
+def simpleArraySum(n, ar):
+    # Complete this function
+
+
+n = int(raw_input().strip())
+ar = map(int, raw_input().strip().split(' '))
+result = simpleArraySum(n, ar)
+print(result)
+

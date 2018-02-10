7fadf4bb38be1cd54b377b571e843202dc0ed764
rodrigolrfm <rodrigotharo123@gmail.com>
2018-02-09 19:00:15 -0500

    RESOLUCION DE FIBONACCI

--- coding_train/basics/fibo.py
+++ coding_train/basics/fibo.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python
+
 import sys
 
-# La linea que esta aqui arriba NO es un comentario, 
-# siempre debe ir en un archivo ejecutable de python
-
-# este archivo se llama exemplo.py
-# lo ejecutas:
-# ./exemplo.py 
-# (tambien se puede ejecutar usando >> python exemplo.py , pero mucha letra para digitar.)
-# si da error es por problemas de permisos del archivo, entonces haz esto:
-# chmod 755 exemplo.py 
-# (este comando chmod es solo necesario ejecutarlo una vez.)
-
-
-
-# esta funcion debe ser implementada
-# estos comentarios deben ser borrados.
-
-def fibonacci(n):
+def fibo(n):
     x = 0
     y = 1
     while x < n:
@@ -26,9 +11,9 @@ def fibonacci(n):
         x = y
         y = z + y
 
-def main():
+def main(): 
     n = 100
-    fibonacci(n)
+    fibo(n)
 
 if  __name__ == "__main__":
     sys.exit(main())
